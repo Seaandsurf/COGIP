8f69755f7d764f3c11b2b408001098afcae7431e
thomas <scozzari.thomas@yahoo.fr>
2024-02-29 17:01:01 +0100

    front last update 2

--- backend/models/connexion.php
+++ backend/models/connexion.php
@@ -9,10 +9,20 @@
 //     return $pdo;
 // }
 
-function connect_db() {
+// function connect_db() {
+//     $dsn = 'mysql:dbname=cogip;host=db';
+//     $user = 'root';
+//     $password = 'KWmuLNjpzseXcTUNcnpz';
+//     $pdo = new PDO($dsn, $user, $password);
+
+//     return $pdo;
+// }
+
+function connect_db()
+{
     $dsn = 'mysql:dbname=cogip;host=db';
     $user = 'root';
-    $password = 'KWmuLNjpzseXcTUNcnpz';
+    $password = 'root';
     $pdo = new PDO($dsn, $user, $password);
 
     return $pdo;
