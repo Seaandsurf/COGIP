b6e50c03ea492521e050b0dc137add2655b5e1d1
thomas <scozzari.thomas@yahoo.fr>
2024-03-11 11:09:02 +0100

    dashboard

---
+++ countries-flag.js
@@ -0,0 +1,19 @@
+fetch("countries-flag.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const selectElement = document.getElementById("countryFlag");
+
+    data.forEach((country) => {
+      const optionElement = document.createElement("option");
+      optionElement.value = country.code;
+      optionElement.textContent = country.country;
+
+      const imgElement = document.createElement("img");
+      imgElement.src = country.flag;
+
+      optionElement.appendChild(imgElement);
+
+      selectElement.appendChild(optionElement);
+    });
+  })
+  .catch((error) => console.error("Error fetching countries.json:", error));
