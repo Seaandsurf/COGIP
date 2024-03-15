3eab508fafa1b934981f96a7284e066c958a0947
Seaandsurf <mathias.hardat@gmail.com>
2024-03-15 14:45:52 +0100

    lissage des pages web pour devellopement

--- backend/controllers/indexControllers.inc.php
+++ backend/controllers/indexControllers.inc.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once(__DIR__ . 'C:/laragon/www/COGIP/backend/controllers/companiesControllers.php');
-require_once(__DIR__ . 'C:/laragon/www/COGIP/backend/controllers/contactsControllers.php');
-require_once(__DIR__ . 'C:/laragon/www/COGIP/backend/controllers/invoicesControllers.php');
-require_once(__DIR__ . 'C:/laragon/www/COGIP/backend/controllers/permissionsControllers.php');
-require_once(__DIR__ . 'C:/laragon/www/COGIP/backend/controllers/rolesControllers.php');
-require_once(__DIR__ . 'C:/laragon/www/COGIP/backend/controllers/typesControllers.php');
-require_once(__DIR__ . 'C:/laragon/www/COGIP/backend/controllers/usersControllers.php');
+require_once 'C:\laragon\www\COGIP\backend\controllers\companiesControllers.php';
+require_once 'C:\laragon\www\COGIP\backend\controllers\contactsControllers.php';
+require_once 'C:\laragon\www\COGIP\backend\controllers\invoicesControllers.php';
+require_once 'C:\laragon\www\COGIP\backend\controllers\permissionsControllers.php';
+require_once 'C:\laragon\www\COGIP\backend\controllers\rolesControllers.php';
+require_once 'C:\laragon\www\COGIP\backend\controllers\typesControllers.php';
+require_once 'C:\laragon\www\COGIP\backend\controllers\usersControllers.php';
 
 function sendJson($data) {
     header('Content-Type: application/json; charset=utf-8');
