4dd3153284776b218dc203046797031f832665a1
Riojardo <149428215+Riojardo@users.noreply.github.com>
2024-03-19 16:05:08 +0100

    ttttt

--- backend/models/connexion.php
+++ backend/models/connexion.php
@@ -1,23 +1,23 @@
 <?php
 // // // thomas, mathias, robin : changer mots de passe
-// function connect_db()
-// {
-//     $host = "localhost";
-//     $database = "cogip";
-//     $username = "root";
-//     $password = "";
-//     $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
-//     return $pdo;
-// }
+function connect_db()
+{
+    $host = "localhost";
+    $database = "cogip";
+    $username = "root";
+    $password = "";
+    $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    return $pdo;
+}
 
 
  
 // alice
-function connect_db() {
-    $dsn = 'mysql:dbname=cogip;host=db';
-    $user = 'root';
-    $password = 'KWmuLNjpzseXcTUNcnpz';
-    $pdo = new PDO($dsn, $user, $password);
+// function connect_db() {
+//     $dsn = 'mysql:dbname=cogip;host=db';
+//     $user = 'root';
+//     $password = 'KWmuLNjpzseXcTUNcnpz';
+//     $pdo = new PDO($dsn, $user, $password);
 
-    return $pdo;
-}
+//     return $pdo;
+// }
